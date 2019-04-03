b962d30c65fc28a97472ebd6da391d18837b1a3e
Walther Chen <walther.chen@gmail.com>
2019-04-03 11:55:22 -0400

    stream blocks using clickhouse fork

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,20 @@
-use futures::Future;
-use clickhouse_rs::{Pool};
+use futures::{self, Future, Stream};
+use clickhouse_rs::Pool;
 
 fn main() -> Result<(), Box<std::error::Error>> {
     let pool = Pool::new("tcp://127.0.0.1:9000");
 
     let fut = pool
         .get_handle()
-        .and_then(|c| {
+        .and_then(move |c| {
             c.query("select * from test")
-                .fold(0, |mut acc, row| {
-                    let pop: Option<i64> = row.get("population")?;
-                    if let Some(pop) = pop {
-                        acc += pop
+                .stream_blocks()
+                .for_each(|block| {
+                    if let Ok(b) = block {
+                        println!("{:?}\nblock row count: {}", b, b.row_count());
                     }
-                    println!("{}", acc);
-                    Ok(acc)
+                    Ok(())
                 })
-        })
-        .and_then(move |(_, i)| {
-            println!("{}", i);
-            Ok(())
         })
         .map_err(|err| eprintln!("database error: {}", err));
 
@@ -27,3 +22,4 @@ fn main() -> Result<(), Box<std::error::Error>> {
 
     Ok(())
 }
+
